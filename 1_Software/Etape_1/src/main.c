943b13e5d26acdbf5e7a4e5705afdc0025e72f71
CHICARD Theo <Theo.Chicard@bordeaux-inp.fr>
2021-02-09 09:00:42 +0100

    Etape_1 OK

--- 1_Software/Etape_1/src/main.c
+++ 1_Software/Etape_1/src/main.c
@@ -2,16 +2,42 @@
 #include "stdlib.h"
 #include "math.h"
 
+
 int PGCD(int A, int B)
 {
-
+	while ( A != B){
+		if (A > B){
+			A = A - B;
+		}else{
+			B = B - A;
+		}
+	}
 	return A;
 }
 
 int main (int argc, char * argv []){
 	printf("(II) Starting PGCD program\n");
+	
+	int a, b;
 
+	if (argc < 3){
+		printf("ERROR : paramètre(s) incorrecte(s)\n\tRAPPEL : ./main a b\n");
+		return 0;
+	}else{
+
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		
+		if ( (0 < a) && (a <= 65535) && (0 < b) && (b <= 65535)){
+			int result = PGCD(a, b);
+			printf("----------Résultat : %u----------\n", result);
+
+		}else{
+			printf("Les valeurs utilisées doivent être comprises dans l'intervalle [1; 65535]\n");
+			return 0;
+		}
+	}
 
 	printf("(II) End of PGCD program\n");
-  return 0;
+  	return 0;
 }
